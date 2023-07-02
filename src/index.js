7835365a1fadcfc10d61f0971e921aebbdc9a749
furyvinay <vinay2557kumar@gmail.com>
2023-07-02 15:38:08 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { dogsRouter } from "./routes/dogs.js";
 
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
@@ -13,13 +14,15 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/dogs", dogsRouter);
 
-
-mongoose.connect(
-  "mongodb+srv://vinay:Vinay2557@cluster0.4cww1ws.mongodb.net/dogs",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-app.listen(3001, () => console.log("Server started at port 3001"));
+mongoose.connect("mongodb+srv://vinay:Vinay2557@cluster0.4cww1ws.mongodb.net/dogs", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server started at port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
